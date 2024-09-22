1fd08df182fced3bfc7a7edcdd4e39dd1cf39c3a
Viktor Svertoka <victor.svertoka@gmail.com>
2024-09-23 00:50:11 +0300

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,34 +1,47 @@
-// ./src/pages/[slug].tsx
-
 import { QueryParams, SanityDocument } from "next-sanity";
 import dynamic from "next/dynamic";
-import { GetStaticPaths } from "next";
-
+import { GetStaticPaths, GetStaticProps } from "next";
 import { getClient } from "../sanity/lib/client";
 import { token } from "../sanity/lib/token";
 import { POSTS_SLUG_QUERY, POST_QUERY } from "../sanity/lib/queries";
 import Post from "@/components/Post";
+import { useRouter } from "next/router";
 
 const PostPreview = dynamic(() => import("@/components/PostPreview"));
 
 type PageProps = {
-  post: SanityDocument;
+  post: SanityDocument | null;
   params: QueryParams;
   draftMode: boolean;
   token: string;
 };
 
-export default function SinglePost(props: PageProps) {
-  return props.draftMode ? (
-    <PostPreview post={props.post} params={props.params} />
-  ) : (
-    <Post post={props.post} />
-  );
+export default function SinglePost({ post, draftMode }: PageProps) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  return draftMode ? <PostPreview post={post} /> : <Post post={post} />;
 }
 
-export const getStaticProps = async ({ params = {}, draftMode = false }) => {
+export const getStaticProps: GetStaticProps = async ({
+  params = {},
+  draftMode = false,
+}) => {
   const client = getClient(draftMode ? token : undefined);
   const post = await client.fetch<SanityDocument>(POST_QUERY, params);
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
@@ -37,10 +50,10 @@ export const getStaticProps = async ({ params = {}, draftMode = false }) => {
       draftMode,
       token: draftMode ? token : "",
     },
+    revalidate: 60,
   };
 };
 
-// Prepare Next.js to know which routes already exist
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = await getClient().fetch(POSTS_SLUG_QUERY);
 
